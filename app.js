ecd406a0ec7a49f2f12bb94de2744f4d16e4f6ab
Kyle Richmond <kylerichmond79@gmail.com>
2017-06-28 11:12:36 -0700

    render function working

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 'use strict';
+var array = [];
+var bradyTable = document.getElementById('bradyTable');
+
+function BradyImage (src, name) {
+  this.src = src;
+  this.name = name;
+  array.push(this);
+};
+new BradyImage ('images/marsha.png', 'marsha');
+new BradyImage ('images/carol.png', 'carol');
+new BradyImage ('images/greg.png', 'greg');
+new BradyImage ('images/jan.png', 'jan');
+new BradyImage ('images/alice.png', 'alice');
+new BradyImage ('images/peter.png', 'peter');
+new BradyImage ('images/cindy.png', 'cindy');
+new BradyImage ('images/mike.png', 'mike');
+new BradyImage ('images/bobby.png', 'bobby');
+function initRender() {
+  console.log('do something!');
+  for (var i = 0; i < array.length; i++) {
+    var imgEl = document.createElement('img');
+    imgEl.src = array[i].src;
+    console.log('image ', imgEl.src);
+    imgEl.id = array[i].name;
+    bradyTable.appendChild(imgEl);
+  }
+}
+initRender();
