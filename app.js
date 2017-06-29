a0efaada0a25e96a78a02dfb1a6099d54b690475
Gabriela Diez <gaby1010@gmail.com>
2017-06-29 10:33:47 -0700

    hotfix with gabby's changes

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 var clickNewGameButton = document.getElementById('new-game');
 var bradyTable = document.getElementById('bradyTable');
 var correctImage = document.getElementById('marsha');
+var incorrectImage = document.getElementById('bradyTable');
+var clock = document.getElementById('clockdiv');
+var secondsSpan = clock.querySelector('.seconds');
 var counter = 0;
 var array = [];
+var interval = null;
 
 function BradyImage (src, name) {
   this.src = src;
@@ -58,9 +62,15 @@ function shuffle() {
 function newGameListener() {
   console.log('Inside newGameListener!');
   clickNewGameButton.addEventListener('click', handleNewGameClick);
+  // clickNewGameButton.addEventListener('click', getTimeRemaining);
+  // clickNewGameButton.addEventListener('click', initializeClock);
 }
 
-// Correct Image Listener
+// function timerListener() {
+//   console.log('Inside timerListener!');
+//   clickNewGameButton.addEventListener('click', getTimeRemaining);
+// }
+
 function correctImageListener() {
   console.log('Inside correctImageListener!');
   correctImage = document.getElementById('marsha');
@@ -71,9 +81,9 @@ function correctImageListener() {
 // Incorrect Image Listener
 function incorrectImageListener() {
   console.log('Inside incorrectImageListener!');
-  carol = document.getElementById('carol');
+  bradyTable = document.getElementById('bradyTable');
   // console.log(correctImage);
-  carol.addEventListener('click', handleIncorrectImageClick);
+  bradyTable.addEventListener('click', handleIncorrectImageClick);
 }
 
 // New Game Handler
@@ -85,6 +95,9 @@ function handleNewGameClick(event) {
   counter = 0;
   shuffle();
   initRender();
+  // getTimeRemaining();
+  initializeClock();
+  // setInterval(initializeClock, 1000);
 }
 
 // Correct Image Handler
@@ -93,7 +106,9 @@ function handleCorrectImageClick(event) {
   event.preventDefault();
   // console.log('Testing event handler!');
   bradyTable.innerHTML = '';
-  counter += 1;
+  // added 2 instead of 1 because clicking anywhere on bradyTable incl Marsha
+  // subtracts 1 from counter
+  counter += 2;
   console.log('Counter is ' + counter);
   shuffle();
   initRender();
@@ -110,6 +125,40 @@ function handleIncorrectImageClick(event) {
   shuffle();
   initRender();
 }
+
+//clock countdown
+function getTimeRemaining(endtime) {
+  console.log('hello');
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor(t / 1000);
+  console.log(t);
+  console.log(seconds);
+  return {
+    'total': t,
+    'seconds': seconds
+  };
+}
+
+function initializeClock() {
+  var endtime = new Date(Date.parse(new Date()) + 60 * 1000);
+  clearInterval(interval);
+
+  console.log('ENDTIME: ', endtime);
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+    console.log(t.total);
+    console.log(t.seconds);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    if (t.seconds <= 0) {
+      clearInterval(interval);
+      console.log('finished');
+    }
+  }
+  interval = setInterval(updateClock, 1);
+}
+
+initializeClock();
 
 initRender();
 newGameListener();
